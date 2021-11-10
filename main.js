53b29def886b8dd31788d7f86d99fd2f4060c1ad
55myClone55 <borismoskalyuk3@gmail.com>
2021-11-10 15:32:17 +0300

    function commit

--- main.js
+++ main.js
@@ -1,70 +1,7 @@
-//let val = ['usa','azerManad','avstalUsa','grivna','bitcoin']
+let items = ['hello', 'world', 23, '23', null]
 
-let currencyFirst = prompt('Введите валюту которая у вас есть','') 
-let figure = +prompt('Сумму в цыфрах (число)','')   
-let currencySecond = prompt('Ту валюту на которую хотите заменить','') 
+function filterBy(arr, type){
+  return  arr.filter(item => typeof item !== type)
+} 
 
-
- if(currencyFirst == 'usa' && currencySecond == 'azerManad'){
-     alert(figure * 1.17) 
-    prompt('Хочешь еще обменять деньги')
- }else if(currencyFirst == 'azerManad' && currencySecond == 'usa'){
-    alert(figure * 0.59) 
-    alert('Хочешь еще обменять деньги')
-}else if(currencyFirst == 'usa' && currencySecond == 'azerManad'){
-    alert(figure * 1.34) 
-   alert('Хочешь еще обменять деньги')
-} else if(currencyFirst == 'avstalUsa' && currencySecond == 'usa'){
-   alert(figure * 0.79) 
-   alert('Хочешь еще обменять деньги')
-}else if(currencyFirst == 'usa' && currencySecond == 'grivna'){
-    alert(figure * 26.3) 
-   alert('Хочешь еще обменять деньги')
-} else if(currencyFirst == 'grivna' && currencySecond == 'usa'){
-   alert(figure * 0.038) 
-   alert('Хочешь еще обменять деньги')
-}else if(currencyFirst == 'usa' && currencySecond == 'bitcoin'){
-    alert(figure * 0.000016) 
-   alert('Хочешь еще обменять деньги')
-} else if(currencyFirst == 'bitcoin' && currencySecond == 'usa'){
-   alert(figure * 64194.1) 
-   alert('Хочешь еще обменять деньги')
-} else if (currencyFirst == 'avstalUsa' && currencySecond == 'grivna'){
-    alert(figure * 19.63) 
-    alert('Хочешь еще обменять деньги')
-}else if( currencyFirst == 'grivna' && currencySecond == 'avstalUsa'){
-        alert(figure * 0.051) 
-        alert('Хочешь еще обменять деньги')
-}else if( currencyFirst == 'azerManad' && currencySecond == 'avstalUsa'){
-        alert(figure * 0.79) 
-        alert('Хочешь еще обменять деньги')
-}else if( currencyFirst == 'avstalUsa' && currencySecond == 'azerManad'){
-        alert(figure * 1.27) 
-        alert('Хочешь еще обменять деньги')
-}else if( currencyFirst == 'bitcoin' && currencySecond == 'avstalUsa'){
-        alert(figure * 84671.56) 
-        alert('Хочешь еще обменять деньги')
-}else if( currencyFirst == 'avstalUsa' && currencySecond == 'bitcoin'){
-    alert(figure * 0.000012) 
-    alert('Хочешь еще обменять деньги')
-}else if( currencyFirst == 'azerManad' && currencySecond == 'bitcoin'){
-    alert(figure * 0.000012) 
-    alert('Хочешь еще обменять деньги')
-}else if( currencyFirst == 'bitcoin' && currencySecond == 'azerManad'){
-    alert(figure * 107312.16) 
-    alert('Хочешь еще обменять деньги')
-}else if( currencyFirst == 'azerManad' && currencySecond == 'grivna'){
-    alert(figure * 15.45) 
-    alert('Хочешь еще обменять деньги')
-}else if( currencyFirst == 'grivna' && currencySecond == 'azerManad'){
-alert(figure * 0.065) 
-alert('Хочешь еще обменять деньги')
-}else if( currencyFirst == 'bitcoin' && currencySecond == 'grivna'){
-    alert(figure * 1660359) 
-    alert('Хочешь еще обменять деньги')
-}else if( currencyFirst == 'grivna' && currencySecond == 'bitcoin'){
-alert(figure * 0.00000062) 
-alert('Хочешь еще обменять деньги')
-}else{
-    alert('Ты что-то не так вводишь')
- }
+console.log(filterBy(items, "string"))
